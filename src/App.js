c4bf62f95e5fb108f3c83f5dc25934c43137f310
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-12-07 14:59:04 -0800

    Todays Study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticleListPage from './pages/ArticleListPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+      <NavBar />
+      <div id='page-body'>
+        <Routes>
+        <Route path ="/" element = {<HomePage />} />
+        <Route path ="/about" element = {<AboutPage />} />
+        <Route path ="/articles" element = {<ArticleListPage />}/>
+        <Route path ="/articles/:articleId" element = {<ArticlePage />} />  
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
