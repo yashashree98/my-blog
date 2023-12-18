80b03e2956ba0d2bf88b937b900fcd78dc2c5204
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-12-18 12:18:24 -0800

    Added created account page

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -1,7 +1,52 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 const CreateAccountPage = () => {
+    const [ email, setEmail ] = useState('');
+    const [password, setPasswod] = useState('');
+    const [ confirmPassword, setConfirmPassword ] = useState('');
+    const [error, setError] = useState('');
+    
+    const navigate = useNavigate();
+
+    const createAccount = async () => {
+        try {
+            if(password !== confirmPassword ){
+                setError('Passwords do not match');
+                return;
+            }
+            await createUserWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return (
+        <>
         <h1>Create Account</h1>
+        { error && <p className="error">{ error }</p>}
+
+        <input 
+            placeholder ="Your email address"
+            value = { email }
+            onChange = { e => setEmail(e.target.value)}/>
+        <input 
+            type="password"
+            placeholder ="Your Password"
+            value = { password }
+            onChange = { e => setPasswod(e.target.value)} />
+        <input 
+            type="password"
+            placeholder ="Re-enter Your Password"
+            value = { confirmPassword }
+            onChange = { e => setConfirmPassword(e.target.value)} />
+        <button onClick={ createAccount }> Create Account</button>
+        <Link to="/create-account"> Already have an account? Log in here!</Link>
+        </>
     );
+
 }
 
 export default CreateAccountPage;
